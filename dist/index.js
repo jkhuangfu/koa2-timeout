d79cb36a67f675cbe6764bba0db48191b374873d
jkhuangfu <huangfujiankang@gmail.com>
2023-04-04 13:19:17 +0800

    -update:0.1.2

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.timeout = void 0;
-function timeout(timeout = 4000, cb) {
+function timeout(time = 4000, cb) {
     let timer;
     return async (ctx, next) => {
         await Promise.race([
             new Promise(resolve => {
                 timer = setTimeout(() => {
                     if (cb) {
-                        cb(ctx);
+                        cb(ctx, next);
                     }
                     else {
                         throw new Error('request timeout');
                     }
                     resolve();
-                }, timeout);
+                }, time);
             }),
             new Promise(resolve => {
                 (async () => {
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,7 @@
-import { Next } from 'koa';
+import type { Context, Next } from 'koa';
 /**
  * @description koa2 response timeout middleware
  * @param {number} timeout (deffault value 4000s) the max time to response
  * @param {function} timeOutCb the function to handle timeout
  */
-export function timeout(timeout: number, timeOutCb?: Function): Promise<(next: Next) => Promise<void>>;
+export function timeout(timeout: number, timeOutCb?: (ctx: Context, next: Next) => void): Promise<(next: Next) => Promise<void>>;
